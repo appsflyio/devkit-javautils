e38bf8888c8e5edae07f011ad713e4ae49754296
Karthik Thirumalasetti <karthik@appsfly.io>
2017-10-07 17:35:37 -0300

    changes

--- src/main/java/io/appsfly/crypto/App.java
+++ src/main/java/io/appsfly/crypto/App.java
@@ -3,7 +3,8 @@ package io.appsfly.crypto;
 public class App {
     public static void main(String args[]){
         String key = "1234567890123456";
-        String checksum = CtyptoUtil.getInstance().getChecksum("{\"hi\":\"hello\"}".getBytes(), key);
+//        String checksum = CtyptoUtil.getInstance().getChecksum("{\"hi\":\"hello\"}".getBytes(), key);
+        String checksum = "TSQmmubgzyBo48lEpTDlVKt+PRM9b64F0NDskym4jInK7KPbiVisU2o734LpRwydWT39VcCww+njhqyL+3WYukkZf9CCeSQ6uW4eWgDSc/0=";
         System.out.println(CtyptoUtil.getInstance().verifychecksum("{\"hi\":\"hello\"}".getBytes(), checksum, key));
     }
 }
--- src/main/java/io/appsfly/crypto/CtyptoUtil.java
+++ src/main/java/io/appsfly/crypto/CtyptoUtil.java
@@ -1,24 +1,9 @@
-/**
- * Copyright 2017 appsfly.io
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 package io.appsfly.crypto;
 
 
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang.ArrayUtils;
 
 import javax.crypto.Cipher;
@@ -45,8 +30,7 @@ public class CtyptoUtil {
 
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
             cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
-
-            byte[] encrypted = cipher.doFinal(data);
+            byte[] encrypted = Base64.encodeBase64(cipher.doFinal(data));
             return encrypted;
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -57,15 +41,14 @@ public class CtyptoUtil {
 
     private byte [] decrypt(byte [] data,String key) {
         try {
-            IvParameterSpec iv = new IvParameterSpec("@@@@&&&&####$$$$".getBytes("UTF-8"));
+            IvParameterSpec iv = new IvParameterSpec("$$appsfly.io##$$".getBytes("UTF-8"));
             SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
 
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 
-            byte[] original = cipher.doFinal(data);
+            return cipher.doFinal(Base64.decodeBase64(data));
 
-            return original;
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -83,37 +66,27 @@ public class CtyptoUtil {
         try {
             MessageDigest digest = MessageDigest.getInstance(algo);
             digest.update(ArrayUtils.addAll(salt, data));
-            return getHexBytes(digest.digest());
+            return digest.digest();
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    private byte[] getHexBytes(byte[] digested) {
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < digested.length; i++) {
-            sb.append(Integer.toString((digested[i] & 0xff) + 0x100, 16).substring(1));
-        }
-        return sb.toString().getBytes();
-    }
-
     private byte[] sha256sum(byte [] salt, byte [] data){
-        return getBytes(salt, data, "SHA-256");
+        return Hex.encodeHexString(getBytes(salt, data, "SHA-256")).getBytes();
     }
 
     public String getChecksum(byte [] data, String key){
         byte[] salt = getSalt(4);
         byte[] encrypt = encrypt(ArrayUtils.addAll(sha256sum(salt, data), salt), key);
-        return new String(Base64.encodeBase64(encrypt));
+        return new String(encrypt);
     }
 
     public boolean verifychecksum(byte [] data, String checksumParam, String key){
-        byte[] checksum = decrypt(Base64.decodeBase64(checksumParam.getBytes()), key);
+        byte[] checksum = decrypt(checksumParam.getBytes(), key);
         byte[] salt = Arrays.copyOfRange(checksum, checksum.length-4, checksum.length);
         byte[] sha256 = Arrays.copyOfRange(checksum, 0, checksum.length-4);
-        System.out.println(new String(salt));
-        System.out.println(new String(sha256));
         return Arrays.equals(sha256,sha256sum(salt,data));
     }
 
