9f8f30cf9640e559690821e42043cf3595485ed5
Mohit Bhansali <mohit@appsfly.io>
2017-11-17 12:03:08 +0530

    Updated return format

--- src/main/java/io/appsfly/core/AppInstance.java
+++ src/main/java/io/appsfly/core/AppInstance.java
@@ -1,7 +1,9 @@
 package io.appsfly.core;
 
 import io.appsfly.crypto.CtyptoUtil;
+import io.appsfly.util.json.JSONArray;
 import io.appsfly.util.json.JSONObject;
+import io.appsfly.util.json.JSONTokener;
 import okhttp3.*;
 
 import java.io.IOException;
@@ -26,24 +28,26 @@ public class AppInstance {
     private String microModuleId;
     public static final MediaType JSON
             = MediaType.parse("application/json; charset=utf-8");
+
     public AppInstance(AFConfig config, String microModuleId) {
         this.config = config;
         this.microModuleId = microModuleId;
     }
 
-    public void exec(final String intent, final JSONObject intentData, final Callback callback){
+    public void exec(final String intent, final JSONObject intentData, final Callback callback) {
         exec(intent, intentData, "generic", callback);
     }
-    public void exec(final String intent, final JSONObject intentData, final String userID, final Callback callback){
+
+    public void exec(final String intent, final JSONObject intentData, final String userID, final Callback callback) {
         final JSONObject body = new JSONObject() {{
             put("intent", intent);
             put("data", intentData);
         }};
-        String payload = body + "|" +  microModuleId + "|" + config.appKey + "|" + userID;
+        String payload = body + "|" + microModuleId + "|" + config.appKey + "|" + userID;
         String checksum = CtyptoUtil.getInstance().getChecksum(payload.getBytes(), config.secretKey);
         OkHttpClient httpClient = new OkHttpClient();
         Request request = new Request.Builder()
-                .url(this.config.repoUrl+"/executor/exec")
+                .url(this.config.repoUrl + "/executor/exec")
                 .addHeader("X-Module-Handle", microModuleId)
                 .addHeader("X-App-Key", config.appKey)
                 .addHeader("X-Checksum", checksum)
@@ -59,22 +63,20 @@ public class AppInstance {
             @Override
             public void onResponse(Call call, final okhttp3.Response response) throws IOException {
                 String checksum = response.headers().get("X-Checksum");
-                if(checksum!=null){
+                if (checksum != null) {
                     byte[] bytes = response.body().bytes();
                     boolean verified = CtyptoUtil.getInstance().verifychecksum(bytes, checksum, config.secretKey);
-                    if (verified){
+                    if (verified) {
                         callback.onResponse(new JSONObject(bytes));
-                    }
-                    else{
-                        callback.onError(new JSONObject(){{
+                    } else {
+                        callback.onError(new JSONObject() {{
                             put("message", "Checksum Validation Failed");
                         }});
                     }
-                }
-                else{
+                } else {
                     final JSONObject responseBody = new JSONObject(response.body().string());
-                    if(responseBody.has("error")){
-                        callback.onError(new JSONObject(){{
+                    if (responseBody.has("error")) {
+                        callback.onError(new JSONObject() {{
                             put("message", responseBody.getJSONObject("error").get("message"));
                             put("status", response.code());
                         }});
@@ -84,20 +86,20 @@ public class AppInstance {
         });
     }
 
-    public JSONObject execSync(final String intent, final JSONObject intentData) throws AppsflyException{
+    public Object execSync(final String intent, final JSONObject intentData) throws AppsflyException {
         return execSync(intent, intentData, "generic");
     }
 
-    public JSONObject execSync(final String intent, final JSONObject intentData, final String userID) throws AppsflyException{
+    public Object execSync(final String intent, final JSONObject intentData, final String userID) throws AppsflyException {
         final JSONObject body = new JSONObject() {{
             put("intent", intent);
             put("data", intentData);
         }};
-        String payload = body + "|" +  microModuleId + "|" + config.appKey + "|" + userID;
+        String payload = body + "|" + microModuleId + "|" + config.appKey + "|" + userID;
         String checksum = CtyptoUtil.getInstance().getChecksum(payload.getBytes(), config.secretKey);
         OkHttpClient httpClient = new OkHttpClient();
         Request request = new Request.Builder()
-                .url(this.config.repoUrl+"/executor/exec")
+                .url(this.config.repoUrl + "/executor/exec")
                 .addHeader("X-Module-Handle", microModuleId)
                 .addHeader("X-App-Key", config.appKey)
                 .addHeader("X-Checksum", checksum)
@@ -107,19 +109,17 @@ public class AppInstance {
         try {
             final Response response = httpClient.newCall(request).execute();
             String responseChecksum = response.headers().get("X-Checksum");
-            if(responseChecksum!=null){
+            if (responseChecksum != null) {
                 byte[] bytes = response.body().bytes();
                 boolean verified = CtyptoUtil.getInstance().verifychecksum(bytes, responseChecksum, config.secretKey);
-                if (verified){
-                    return new JSONObject(new String(bytes));
-                }
-                else{
+                if (verified) {
+                    return new JSONTokener(new String(bytes)).nextValue();
+                } else {
                     throw new AppsflyException("Checksum Validation Failed");
                 }
-            }
-            else{
+            } else {
                 final JSONObject responseBody = new JSONObject(response.body().string());
-                if(responseBody.has("error")){
+                if (responseBody.has("error")) {
                     throw new AppsflyException(responseBody.getJSONObject("error").getString("message"));
                 } else {
                     return responseBody;
