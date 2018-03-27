5e14ac3d64d235190b5bafb44aeba55f93d68f85
Srikanth Akula <srikanth@appsfly.io>
2018-03-27 19:35:45 +0530

    changes

--- src/main/java/io/appsfly/sample/App.java
+++ src/main/java/io/appsfly/sample/App.java
@@ -9,12 +9,12 @@ public class App {
     public static void main(String args[]) {
         org.apache.log4j.BasicConfigurator.configure();
 
-        AppInstance.AFConfig secureConfig = new AppInstance.AFConfig("https://microapps.appsfly.io", "3384354330428323", "abb3f71c-a8cc-4f2a-90aa-23ac3771f5f7");
-//        AppInstance.AFConfig InSecureConfig = new AppInstance.AFConfig("https://microapps.appsfly.io", null, "abb3f71c-a8cc-4f2a-90aa-23ac3771f5f7");
-//        AppInstance clearTrip = new AppInstance(InSecureConfig
-// , "io.appsfly.msctpactivities");
-        AppInstance clearTrip = new AppInstance(secureConfig, "io.appsfly.msctpactivities");
-        clearTrip.exec("fetch_cities", new JSONObject() {{
+        /**
+         *Plain Request
+         */
+        AppInstance.AFConfig InSecureConfig = new AppInstance.AFConfig("https://microapps.appsfly.io", null, "abb3f71c-a8cc-4f2a-90aa-23ac3771f5f7");
+        AppInstance clearTripInsecure = new AppInstance(InSecureConfig, "io.appsfly.msctpactivities");
+        clearTripInsecure.exec("fetch_cities", new JSONObject() {{
         }}, new Callback() {
             @Override
             public void onResponse(Object response) {
@@ -29,23 +29,25 @@ public class App {
             }
         });
 
+/**
+ *JWT Request
+ */
+        AppInstance.AFConfig secureConfig = new AppInstance.AFConfig("https://microapps.appsfly.io", "3384354330428323", "abb3f71c-a8cc-4f2a-90aa-23ac3771f5f7");
+        AppInstance clearTripSecure = new AppInstance(secureConfig, "io.appsfly.msctpactivities");
+        clearTripSecure.exec("fetch_cities", new JSONObject() {{
+        }}, new Callback() {
+            @Override
+            public void onResponse(Object response) {
+                System.out.println(response.toString());
+                // Payment Done Response
+                // We have already verified the checksum from you
+            }
 
-//        AppInstanceJwt bookMyShow = new AppInstanceJwt(config, "com.cleartrip.microservices.default");
-//        bookMyShow.exec("doBooking",new JSONObject() {{
-//            //Set Params Here
-//            // We will take care of checksum
-//        }}, new Callback() {
-//            @Override
-//            public void onResponse(JSONObject response) {
-//                // Payment Done Response
-//                // We have already verified the checksum from you
-//            }
-//
-//            @Override
-//            public void onError(JSONObject error) {
-//                // On Error
-//            }
-//        });
+            @Override
+            public void onError(JSONObject error) {
+                System.out.println(error);
+            }
+        });
 
 
     }
